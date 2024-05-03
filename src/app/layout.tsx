8b5333ba6bb3b4e56a90499fc4acf9722c40f839
BahaaAldein <BahaaAldein@gmail.com>
2024-05-03 09:14:36 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
         <div className="flex flex-col h-screen">
           <Navbar />
           <main className="flex-1">{children}</main>
-
           <Footer />
         </div>
 
